78f5f5ea81feee3a799a4a03dc32221a227ed0f3
b-ambino <s3661473@student.rmit.edu.au>
2024-05-12 12:46:48 +1000

    added week 7

---
+++ week7/script.js
@@ -0,0 +1,33 @@
+const airportAudio = document.querySelector("#airport-audio");
+console.log(airportAudio);
+// airportAudio.removeAttribute
+
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
+
+playButton.addEventListener("click", playAirportAudio);
+
+function playAirportAudio() {
+  airportAudio.play();
+}
+
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+
+pauseButton.addEventListener("click", pauseAirportAudio);
+
+function pauseAirportAudio() {
+  airportAudio.pause();
+}
+
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+
+const popSound = document.querySelector("#pop-sound");
+console.log(popSound);
+
+popButton.addEventListener("click", popIt);
+
+function popIt() {
+  popSound.play();
+}
